3033deca2e11c8c41ff01f395bc744d98b413491
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-02-11 00:20:13 +0300

    Create CharacterResponse

---
+++ app/src/main/java/com/hercan/motionlayoutexample/core/network/dto/CharacterResponse.kt
@@ -0,0 +1,15 @@
+package com.hercan.motionlayoutexample.core.network.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class CharacterResponse(
+    @SerializedName("creator") val creator: List<String?>?,
+    @SerializedName("episodes") val episodes: Int?,
+    @SerializedName("genre") val genre: List<String?>?,
+    @SerializedName("id") val id: Int?,
+    @SerializedName("image") val image: String?,
+    @SerializedName("rating") val rating: String?,
+    @SerializedName("runtime_in_minutes") val runtimeInMinutes: Int?,
+    @SerializedName("title") val title: String?,
+    @SerializedName("year") val year: Int?
+)
