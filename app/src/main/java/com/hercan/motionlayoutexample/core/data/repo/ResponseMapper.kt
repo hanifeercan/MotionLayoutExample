1e2ffcc4370b167720745e225781f72aa96168ad
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-02-11 00:26:56 +0300

    Create MapExtensions an ResponseMapper

---
+++ app/src/main/java/com/hercan/motionlayoutexample/core/data/repo/ResponseMapper.kt
@@ -0,0 +1,21 @@
+package com.hercan.motionlayoutexample.core.data.repo
+
+import com.hercan.motionlayoutexample.core.data.model.CharacterModel
+import com.hercan.motionlayoutexample.core.network.dto.CharacterResponse
+import retrofit2.Response
+
+typealias RestResponse = Response<List<CharacterResponse>>
+
+fun RestResponse.toCharacterModelList(): List<CharacterModel> {
+    return body()!!.map { character ->
+        CharacterModel(
+            creator = character.creator,
+            episodes = character.episodes,
+            id = character.id,
+            image = character.image,
+            runtimeInMinutes = character.runtimeInMinutes,
+            title = character.title,
+            year = character.year
+        )
+    }
+}
