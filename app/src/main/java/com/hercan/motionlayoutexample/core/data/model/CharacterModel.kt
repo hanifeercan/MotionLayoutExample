88fbd69382a925b32feb30b4d950557957eef44d
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-02-11 00:26:20 +0300

    Create CharacterModel

---
+++ app/src/main/java/com/hercan/motionlayoutexample/core/data/model/CharacterModel.kt
@@ -0,0 +1,11 @@
+package com.hercan.motionlayoutexample.core.data.model
+
+data class CharacterModel(
+    val creator: List<String?>?,
+    val episodes: Int?,
+    val id: Int?,
+    val image: String?,
+    val runtimeInMinutes: Int?,
+    val title: String?,
+    val year: Int?
+)
