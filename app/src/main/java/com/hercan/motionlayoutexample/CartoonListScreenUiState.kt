cc9f63f130a97ec37b8ae477dde58d11d6ad9d2d
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-02-12 00:43:06 +0300

    Rename CharacterListScreenUiState to CartoonListScreenUiState

--- app/src/main/java/com/hercan/motionlayoutexample/CartoonListScreenUiState.kt
+++ app/src/main/java/com/hercan/motionlayoutexample/CartoonListScreenUiState.kt
@@ -2,13 +2,13 @@ package com.hercan.motionlayoutexample
 
 import com.hercan.motionlayoutexample.core.data.model.Cartoon
 
-data class CharacterListScreenUiState(
+data class CartoonListScreenUiState(
     val characters: List<Cartoon> = emptyList(),
     val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String? = ""
 ) {
     companion object {
-        fun initial() = CharacterListScreenUiState(isLoading = true)
+        fun initial() = CartoonListScreenUiState(isLoading = true)
     }
 }
--- app/src/main/java/com/hercan/motionlayoutexample/MainActivityViewModel.kt
+++ app/src/main/java/com/hercan/motionlayoutexample/MainActivityViewModel.kt
@@ -1,22 +1,22 @@
 package com.hercan.motionlayoutexample
 
-import androidx.lifecycle.ViewModel
-import com.hercan.motionlayoutexample.core.domain.GetCharactersUseCase
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.hercan.motionlayoutexample.core.common.ResponseState
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.hercan.motionlayoutexample.core.common.ResponseState
+import com.hercan.motionlayoutexample.core.domain.GetCharactersUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
     private val getCharactersUseCase: GetCharactersUseCase
 ) : ViewModel() {
 
-    private val _characterScreenUiState = MutableLiveData(CharacterListScreenUiState.initial())
-    val characterScreenUiState: LiveData<CharacterListScreenUiState> get() = _characterScreenUiState
+    private val _characterScreenUiState = MutableLiveData(CartoonListScreenUiState.initial())
+    val characterScreenUiState: LiveData<CartoonListScreenUiState> get() = _characterScreenUiState
 
     fun getAllBooks() {
         viewModelScope.launch {
@@ -24,18 +24,18 @@ class MainActivityViewModel @Inject constructor(
                 when (responseState) {
                     is ResponseState.Error -> {
                         _characterScreenUiState.postValue(
-                            CharacterListScreenUiState(
+                            CartoonListScreenUiState(
                                 isError = true, errorMessage = responseState.message
                             )
                         )
                     }
 
                     is ResponseState.Loading -> {
-                        _characterScreenUiState.postValue(CharacterListScreenUiState(isLoading = true))
+                        _characterScreenUiState.postValue(CartoonListScreenUiState(isLoading = true))
                     }
 
                     is ResponseState.Success -> {
-                        _characterScreenUiState.postValue(CharacterListScreenUiState(responseState.data))
+                        _characterScreenUiState.postValue(CartoonListScreenUiState(responseState.data))
                     }
                 }
             }
